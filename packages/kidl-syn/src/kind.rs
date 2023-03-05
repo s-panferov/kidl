a41de956ba06e69bd04a3dc3386161c96dc62611
Stanislav Panferov <stanislav@panferov.dev>
2023-03-04 22:05:40 -0800

    Introduce tokens

---
+++ packages/kidl-syn/src/kind.rs
@@ -0,0 +1,60 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u16)]
+pub enum SyntaxKind {
+    Token(TokenKind),
+    Node(NodeKind),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u16)]
+pub enum TokenKind {
+    CurlyOpen = 0,
+    CurlyClose,
+    SquareOpen,
+    SquareClose,
+    AngleOpen,
+    AngleClose,
+    ParenOpen,
+    ParenClose,
+    Comment,
+    Comma,
+    Dot,
+    Hash,
+    Question,
+    Eq,
+    Colon,
+    Semicolon,
+    Space,
+    NewLine,
+    Ident,
+    String,
+    Number,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u16)]
+pub enum NodeKind {
+    Root = 1000,
+    Error,
+}
+
+impl From<SyntaxKind> for rowan::SyntaxKind {
+    fn from(kind: SyntaxKind) -> Self {
+        match kind {
+            SyntaxKind::Token(t) => rowan::SyntaxKind(t as u16),
+            SyntaxKind::Node(n) => rowan::SyntaxKind(n as u16),
+        }
+    }
+}
+
+impl From<NodeKind> for rowan::SyntaxKind {
+    fn from(kind: NodeKind) -> Self {
+        rowan::SyntaxKind(kind as u16)
+    }
+}
+
+impl From<TokenKind> for rowan::SyntaxKind {
+    fn from(kind: TokenKind) -> Self {
+        rowan::SyntaxKind(kind as u16)
+    }
+}
